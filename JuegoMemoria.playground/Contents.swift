9b6163d1eb1bfd141154ba1dd3e6115f92c675dd
manuel Cepeda <mac@macbook-pro-de-mac.local>
2016-01-28 22:48:51 -0500

    Version Final ejercicion modulo2

--- JuegoMemoria.playground/Contents.swift
+++ JuegoMemoria.playground/Contents.swift
@@ -2,4 +2,19 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var rango = 0...100
+
+for i in rango{
+    if i%5 == 0{
+        print("\(i) el número \"Bingo!!!\"")
+    }
+    if i%2 == 0{
+        print("\(i) el número \"par!!!\"")
+    }
+    if i%2 != 0{
+        print("\(i) el número \"impar!!!\"")
+    }
+    if i >= 30 && i <= 40{
+        print("\(i) el número \"Viva Swift!!!\"")
+    }
+}
