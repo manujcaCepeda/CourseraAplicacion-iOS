9e1b0a7a3be556597089a97858ee94f257eea7fb
manuel Cepeda <mac@macbook-pro-de-mac.local>
2016-01-28 22:18:03 -0500

    Ejercicio modulo 4 completo

--- VelocimetroAutomovil.playground/Contents.swift
+++ VelocimetroAutomovil.playground/Contents.swift
@@ -2,4 +2,50 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum Velocidades : Int{
+    case Apagado = 0,
+        VelocidadBaja = 20,
+        VelocidadMedia = 50,
+        VelocidadAlta = 120
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+
+class Auto {
+    var velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
+    
+    init(){
+        velocidad = Velocidades.Apagado
+    }
+    
+    func cambioDeVelocidad() ->(actual:Int, velocidadenCadena:String){
+        var mensaje = ""
+        var resultado = (1, "")
+        if(velocidad == Velocidades.Apagado){
+            mensaje = "Apagado"
+            resultado = (velocidad.rawValue, mensaje)
+            velocidad = Velocidades.VelocidadBaja
+        }else if(velocidad == Velocidades.VelocidadBaja){
+            mensaje = "Velocidad baja"
+            resultado = (velocidad.rawValue, mensaje)
+            velocidad = Velocidades.VelocidadMedia
+        }else if(velocidad == Velocidades.VelocidadMedia){
+            mensaje = "Velocidad media"
+            resultado = (velocidad.rawValue, mensaje)
+            velocidad = Velocidades.VelocidadAlta
+        }else if(velocidad == Velocidades.VelocidadAlta){
+            mensaje = "Velocidad alta"
+            resultado = (velocidad.rawValue, mensaje)
+            velocidad = Velocidades.VelocidadMedia
+        }        
+        return resultado
+    }
+    
+}
+var auto = Auto()
+for i in 1...20{
+    var resultado = auto.cambioDeVelocidad()
+    print("\(i). \(resultado.actual), \(resultado.velocidadenCadena)")
+}
